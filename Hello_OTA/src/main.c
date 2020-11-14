522ea2da4671fa000d018e308f1bd33710617d9c
Beshoy Wesa <45521942+be4oo@users.noreply.github.com>
2020-11-14 02:03:47 +0200

    add Get_Record function and some comments

--- Hello_OTA/src/main.c
+++ Hello_OTA/src/main.c
@@ -16,15 +16,20 @@
 #include "FPEC_interface.h"
 #include "ESP_interface.h"
 
-
-void Parser_voidParseRecord(u8* Copy_u8BufData);
+void Get_Record(void);
 
 volatile u8  u8RecBuffer[100]   ;
 volatile u8  u8RecCounter    = 0;
 volatile u8  u8TimeOutFlag   = 0;
-volatile u16 u16TimerCounter = 0;
 volatile u8  u8BLWriteReq    = 1;
+
+/* var for control and clean the record */
 volatile u8  *HEXFile;
+volatile u32 line = 0;
+u8 ipd = 1;
+volatile u8 bufcount = 0;
+volatile u8 last = 0;
+
 typedef void (*Function_t)(void);
 Function_t addr_to_call = 0;
 
@@ -41,8 +46,6 @@ void func(void)
 
 void main(void)
 {
-	u8 Local_u8RecStatus;
-
 	RCC_voidInitSysClock(); /* HSI */
 
 	/* ESP ENABLE */
@@ -60,7 +63,7 @@ void main(void)
 	MGPIO_voidSetPinDirection(GPIOA, PIN0, OUTPUT_SPEED_2MHZ_PP);
 
 
-	MUART_voidInit(); /* ***9600 */
+	MUART_voidInit(); /* 115200 */
 
 	MSTK_voidInit();
 
@@ -68,20 +71,14 @@ void main(void)
 	ESP_WIFIConnect();
 
 	ESP_voidSendData();
+
+	/* getting the uploaded file array on HEXFile pointer */
 	HEXFile = ESP_u8WebState();
 
-	//volatile u8 i = *(HEXFile+10000);
-
-	//MSTK_voidSetIntervalSingle(60000000,func);
-
-	volatile u32 line = 0;
-	u8 ipd = 1;
-	volatile u8 bufcount = 0;
-	volatile u8 last = 0;
-
+	/* to know your start point of the record */
 	if(ipd == 1)
 	{
-		for(u8 i = 0 ; i < 100 ; i++)
+		for(u8 i = 0 ; i < 200 ; i++)
 		{
 			if(*(HEXFile+i) == 'I' && *(HEXFile+(i+1)) == 'P' && *(HEXFile+(i+2)) == 'D')
 			{
@@ -93,87 +90,11 @@ void main(void)
 	}
 
 
-	while(u8TimeOutFlag == 0)
+	while(1)
 	{
 
-		//Local_u8RecStatus = MUSART1_u8Receive( &(u8RecBuffer[u8RecCounter]) );
+		Get_Record();
 
-		Local_u8RecStatus = 1;
-		if (Local_u8RecStatus == 1)
-		{
-			//MSTK_voidStopInterval();
-
-
-			for(u16 j = line ; j < 13060 ; j++)
-			{
-				if(*(HEXFile+j) == '\n' && *(HEXFile+(j+1)) != '+')
-				{
-					line = j+1;
-					while(bufcount !=99)
-					{
-						u8RecBuffer[bufcount] = 0;
-						bufcount++;
-					}
-					break;
-				}
-				/*
-				else if(*(HEXFile+j) == ':')
-				{
-					continue;
-				}
-				*/
-				else
-				{
-					if(*(HEXFile+(j)) == '+' && *(HEXFile+(j+1)) == 'I')
-					{
-						j = j + 9;
-						bufcount = bufcount - 2;
-						continue;
-					}
-					else
-					{
-						if(*(HEXFile+(j)) == 'C' && *(HEXFile+(j+1)) == 'L' && *(HEXFile+(j+2)) == 'O' && *(HEXFile+(j+3)) == 'S')
-						{
-							last = 1;
-							break;
-						}
-
-						u8RecBuffer[bufcount] = *(HEXFile+(j));
-						bufcount++;
-
-					}
-				}
-			}
-
-			/*
-			for(u16 j = line ; j < 13400 ; j++)
-			{
-				if(*(HEXFile+(j+1)) == ':' && *(HEXFile+(j+8)) == '4')
-				{
-					re = j+1;
-					while(re != j+9)
-					{
-						u8RecBuffer[bufcount] = *(HEXFile+re);
-						bufcount++;
-						re++;
-					}
-					line = (j+15);
-					break;
-				}
-				if(*(HEXFile+j) == ':' && *(HEXFile+(j+43)) == '\n')
-				{
-					re = j;
-					while(re != j+43)
-					{
-						u8RecBuffer[bufcount] = *(HEXFile+re);
-						bufcount++;
-						re++;
-					}
-					line = (j+43);
-					break;
-				}
-			}
-			*/
 				if (u8BLWriteReq == 1)
 				{
 					 FPEC_voidEraseAppArea();
@@ -182,21 +103,61 @@ void main(void)
 
 				/* Parse */
 				Parser_voidParseRecord(u8RecBuffer);
-				//MUSART1_voidTransmit("ok");
 				bufcount = 0;
 
 
 				if(last == 1)
 				{
-					//u8TimeOutFlag = 1;
 					func();
 				}
 
+	}
+}
+
+
+
+void Get_Record(void)
+{
+	for(u16 j = line ; j < 25000 ; j++)
+	{
+		if(*(HEXFile+j) == '\n' && *(HEXFile+(j+1)) != '+')
+		{
+			line = j+1;
+			while(bufcount !=99)
+			{
+				u8RecBuffer[bufcount] = 0;
+				bufcount++;
 			}
-
-
-			//MSTK_voidSetIntervalSingle(15000000,func);
+			break;
 		}
+		/*
+		else if(*(HEXFile+j) == ':')
+		{
+			continue;
+		}
+		*/
+		else
+		{
+			if(*(HEXFile+(j)) == '+' && *(HEXFile+(j+1)) == 'I')
+			{
+				j = j + 9;
+				bufcount = bufcount - 2;
+				continue;
+			}
+			else
+			{
+				if(*(HEXFile+(j)) == 'C' && *(HEXFile+(j+1)) == 'L' && *(HEXFile+(j+2)) == 'O' && *(HEXFile+(j+3)) == 'S')
+				{
+					last = 1;
+					break;
+				}
+
+				u8RecBuffer[bufcount] = *(HEXFile+(j));
+				bufcount++;
+
+			}
+		}
+	}
 
 
 }
